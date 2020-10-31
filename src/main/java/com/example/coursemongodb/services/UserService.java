399f6e8b2cb56353ce46af57dc624275ff6cd1f0
alexandredalquieco <alexandre.dalquieco@gmail.com>
2020-10-31 11:46:00 -0300

    News Methods

--- src/main/java/com/example/coursemongodb/services/UserService.java
+++ src/main/java/com/example/coursemongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.coursemongodb.domain.User;
+import com.example.coursemongodb.dto.UserDTO;
 import com.example.coursemongodb.repository.UserRepository;
 import com.example.coursemongodb.services.exception.ObjectNotFoundException;
 
@@ -25,4 +26,28 @@ public class UserService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 		}
 
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(),objDto.getName(),objDto.getEmail());
+		
+	}
 }
